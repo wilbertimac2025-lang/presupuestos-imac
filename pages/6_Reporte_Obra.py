838f3b8f929f6a0f52df9ac8da1e97bc85333630
wilbertimac2025-lang <wilbertimac2025@gmail.com>
2026-06-04 16:41:20 -0600

    Update 6_Reporte_Obra.py

--- pages/6_Reporte_Obra.py
+++ pages/6_Reporte_Obra.py
@@ -7,7 +7,7 @@ import pandas as pd
 st.set_page_config(page_title="Reporte de Obra", page_icon="📑", layout="wide")
 
 # 🔐 CONTRASEÑA MAESTRA
-CLAVE_ADMIN = "IMACADMIN"
+CLAVE_ADMIN = "2289"
 
 def limpiar_monto(valor):
     if str(valor).strip() == "" or valor is None: return 0.0
